b02170ccf3bb4a3c37dced25c5f41f81a8b7857c
Dhanu M <dhanushpriya119@gmail.com>
2026-08-03 10:36:59 +0530

    Update cluster-vm.tf

--- Terraform/ec2/cluster-vm.tf
+++ Terraform/ec2/cluster-vm.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "master" {
     ami = ""
-    instance_type = "t2.medium"
+    instance_type = "t3.micro"
     key_name = aws_key_pair.key.key_name
     subnet_id = var.private_sub_1
     vpc_security_group_ids = [var.k8s_master_sg]
@@ -12,7 +12,7 @@ resource "aws_instance" "master" {
 
 resource "aws_instance" "worker" {
     ami = ""
-    instance_type = "t2.medium"
+    instance_type = "t3.micro"
     key_name = aws_key_pair.key.key_name
     subnet_id = var.private_sub_2
     vpc_security_group_ids = [var.k8s_worker_sg]
